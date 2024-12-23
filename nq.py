39e924937f3442c893283ebcd8435b3e6ee718c7
Hyun Seok Yoon <hyunseoky9@gmail.com>
2024-12-23 11:47:19 -0700

    added episode done handling in nq

--- nq.py
+++ nq.py
@@ -13,19 +13,23 @@ class nq:
     def add(self, state, action, reward, next_state, done, memory, per):
         # Add to n-step queue
         self.queue.append((state, action, reward, next_state, done))
-        
+     
         # If n-step queue is ready, calculate n-step return
         if len(self.queue) >= self.n:
-            G = sum([self.gamma**i * self.queue[i][2] for i in range(self.n)])  # Accumulate rewards
-            state, action, _, _, _ = self.queue[0]  # Take the first state-action pair
-            _, _, _, next_state, done = self.queue[-1]  # Take the last next_state and done
-            if per: # prioritized experience replay
-                memory.add(memory.max_abstd, (state, action, reward, next_state, done)) # add experience to memory
-            else: # vanilla experience replay
-                memory.add(state, action, reward, next_state, done) # add experience to memory
+            self.add2mem(memory, per)
+        
+        # If the episode is done, clear the n-step queue
+        if done:
+            while len(self.queue) > 0:
+                self.add2mem(memory, per)
+            self.queue = [] # make sure the queue is cleared after the episode is done
             
-            # Remove the oldest transition from the n-step queue
-            self.queue.pop(0)
-        # Maintain buffer size
-        if len(self.memory) > self.capacity:
-            self.memory.pop(0)
+    def add2mem(self, memory, per):
+        G = sum([self.gamma**i * self.queue[i][2] for i in range(self.n)])  # Accumulate rewards
+        state, action, _, _, _ = self.queue[0]  # Take the first state-action pair
+        _, _, _, next_state, done = self.queue[-1]  # Take the last next_state and done
+        if per: # prioritized experience replay
+            memory.add(memory.max_abstd, (state, action, G, next_state, done)) # add experience to memory
+        else: # vanilla experience replay
+            memory.add(state, action, G, next_state, done) # add experience to memory
+        self.queue.pop(0) # Remove the oldest transition from the n-step queue
