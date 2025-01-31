4c50cfcd61d578eb22b783890b43c51494083070
Hyun Seok Yoon <hyunseoky9@gmail.com>
2025-01-30 22:38:54 -0700

    ready

--- script2.sh
+++ script2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=maxFib      ## Name of the job
-#SBATCH --output=maxFib.out    ## Output file
+#SBATCH --job-name=tigerPOMDP      ## Name of the job
+#SBATCH --output=tigerPOMDP.out    ## Output file
 #SBATCH --time=10:00           ## Job Duration
-#SBATCH --ntasks=2             ## Number of tasks (analyses) to run
-#SBATCH --cpus-per-task=1      ## The number of threads the code will use
+#SBATCH --ntasks=1             ## Number of tasks (analyses) to run
+#SBATCH --cpus-per-task=8      ## The number of threads the code will use
 #SBATCH --mem-per-cpu=2G     ## Real memory(MB) per CPU required by the job.
 
 ## Load the python interpreter
@@ -13,7 +13,6 @@ module purge
 
 ## Execute the python script and pass the argument/input '90'
 source ~/miniconda3/bin/activate py310
-srun --exclusive -n1 python script.py 90 &
-srun --exclusive -n1 python script.py 100 &
+srun --exclusive -n1 python DRQN_tiger_main.py 1 &
 
 wait
