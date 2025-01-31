5c2273ab0bf846a0a44ccb0187d20fb281cac19e
Hyun Seok Yoon <hyunseoky9@gmail.com>
2025-01-30 17:28:34 -0700

    script modified

--- script2.sh
+++ script2.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=maxFib      ## Name of the job
 #SBATCH --output=maxFib.out    ## Output file
 #SBATCH --time=10:00           ## Job Duration
-#SBATCH --ntasks=1             ## Number of tasks (analyses) to run
+#SBATCH --ntasks=2             ## Number of tasks (analyses) to run
 #SBATCH --cpus-per-task=1      ## The number of threads the code will use
 #SBATCH --mem-per-cpu=2G     ## Real memory(MB) per CPU required by the job.
 
@@ -13,4 +13,7 @@ module purge
 
 ## Execute the python script and pass the argument/input '90'
 source ~/miniconda3/bin/activate py310
-srun python script.py 90
+srun --exclusive -n1 python script.py 90 &
+srun --exclusive -n1 python script.py 100 &
+
+wait
