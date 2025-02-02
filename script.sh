03f4ef860ea2dd0e9adb9ca3e96c6179990d5535
Hyun Seok Yoon <hyunseoky9@gmail.com>
2025-02-02 13:34:30 -0700

    time extended..

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=tigerPOMDP0      ## Name of the job
 #SBATCH --output=tigerPOMDP0.out    ## Output file
-#SBATCH --time=20:00           ## Job Duration
+#SBATCH --time=05:20:00           ## Job Duration
 #SBATCH --ntasks=1             ## Number of tasks (analyses) to run
 #SBATCH --cpus-per-task=8      ## The number of threads the code will use
 #SBATCH --mem-per-cpu=2G     ## Real memory(MB) per CPU required by the job.
