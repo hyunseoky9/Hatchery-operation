86fa90e52b30ef892fc420cfbd6f5ed935310fcd
Hyun Seok Yoon <hyunseoky9@gmail.com>
2025-02-02 00:11:43 -0700

    commit

--- script.sh
+++ script.sh
@@ -2,14 +2,13 @@
 
 #SBATCH --job-name=tigerPOMDP0      ## Name of the job
 #SBATCH --output=tigerPOMDP0.out    ## Output file
-#SBATCH --time=03:00           ## Job Duration
-#SBATCH --ntasks=10             ## Number of tasks (analyses) to run
+#SBATCH --time=20:00           ## Job Duration
+#SBATCH --ntasks=1             ## Number of tasks (analyses) to run
 #SBATCH --cpus-per-task=8      ## The number of threads the code will use
 #SBATCH --mem-per-cpu=2G     ## Real memory(MB) per CPU required by the job.
 
 ## Load the python interpreter
 module purge
-
 
 ## Execute the python script and pass the argument/input '90'
 source ~/miniconda3/bin/activate py310
