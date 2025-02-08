6cb00ecf3462716b78a5d4eb321d6d52da198a37
Hyun Seok Yoon <hyunseoky9@gmail.com>
2025-02-07 17:21:03 -0700

    testing for

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
+import numpy as np
 import torch
+from env2_1 import Env2_1
+from choose_action import choose_action
+from choose_action_a3c import choose_action_a3c
+from calc_performance import calc_performance
+optim_method = [0, 66102, 36]
+env = Env2_1([-1,-1,-1,-1,-1,-1],2,0)
+wd = f'./DRQN results/seed{optim_method[1]}_paramset{optim_method[2]}'
+method_str = 'DRQN'
+filename= f"{wd}/bestQNetwork_{env.envID}_par{env.parset}_dis{env.discset}_DRQN.pt"
+# if you want to test with the best Q network, comment out the next two lines
+episodenum = 2900
+filename= f"{wd}/QNetwork_{env.envID}_par{env.parset}_dis{env.discset}_DRQN_episode{episodenum}.pt"
+Q = torch.load(filename)
 
-print('loading torch properly')
+device=  'cpu'
+print(calc_performance(env, device, Q, policy=None, episodenum=1000, t_maxstep = 100, drqn=True, actioninput=False))
