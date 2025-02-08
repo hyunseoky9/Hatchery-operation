0887ede4f3725a44da1c116199c7e9073aef6fb7
Hyun Seok Yoon <hyunseoky9@gmail.com>
2025-02-07 20:44:00 -0700

    heye

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ from env2_1 import Env2_1
 from choose_action import choose_action
 from choose_action_a3c import choose_action_a3c
 from calc_performance import calc_performance
-optim_method = [0, 66102, 36]
+optim_method = [0, 66102, 34]
 env = Env2_1([-1,-1,-1,-1,-1,-1],2,0)
 wd = f'./DRQN results/seed{optim_method[1]}_paramset{optim_method[2]}'
 method_str = 'DRQN'
 filename= f"{wd}/bestQNetwork_{env.envID}_par{env.parset}_dis{env.discset}_DRQN.pt"
 # if you want to test with the best Q network, comment out the next two lines
-episodenum = 2900
+episodenum = 4400
 filename= f"{wd}/QNetwork_{env.envID}_par{env.parset}_dis{env.discset}_DRQN_episode{episodenum}.pt"
 Q = torch.load(filename,weights_only=False)
 
